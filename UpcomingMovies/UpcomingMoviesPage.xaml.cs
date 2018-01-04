78e5f2a4e252f6c68f076bbbcb526059026c4a44
Guillermo Tinoco <m3mo89@gmail.com>
2018-01-04 10:06:16 -0600

    Initial Commit

---
+++ UpcomingMovies/UpcomingMoviesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace UpcomingMovies
+{
+    public partial class UpcomingMoviesPage : ContentPage
+    {
+        public UpcomingMoviesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
